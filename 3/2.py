f17010291a496e5ac63943f1bea72c84d674b002
Simon <41542666+Zigolox@users.noreply.github.com>
2020-12-03 15:00:29 +0000

    Update 2.py

--- 3/2.py
+++ 3/2.py
@@ -2,7 +2,7 @@ with open("input.txt", "r") as karta:
 	multiply = 1
 	map = [line.strip() for line in karta]
 	for i, j in [(1, 1),(3, 1),(5, 1),(7, 1),(1, 2)]:	
-		x = y =trees = 0
+		x = y = trees = 0
 		while y < len(map):
 			if map[y][x] == '#':
 				trees += 1
