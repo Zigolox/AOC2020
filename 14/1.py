d8d7338a0b21c13abee1886cf4136212192109a1
Simon <41542666+Zigolox@users.noreply.github.com>
2020-12-15 23:24:06 +0100

    must be fixed

--- 14/1.py
+++ 14/1.py
@@ -7,5 +7,4 @@ with open("input.txt", "r") as config:
             ab = int(t[2].replace('X','1'),2)
         else:
             a[t[0]] = int(t[2]) & ab | ob
-            print(a)
     print(sum([a[i] for i in a]))
