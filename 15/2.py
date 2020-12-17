b62a0540b69efb73e6a6df5c88646270b61fb228
Simon <41542666+Zigolox@users.noreply.github.com>
2020-12-18 00:34:30 +0100

    15

---
+++ 15/2.py
@@ -0,0 +1,15 @@
+with open("input.txt",'r') as start:
+    s = [int(i) for i in start.readlines()[0].split(',')]
+    seen = {}
+    for i,j in enumerate(s[:-1]):
+        seen[j] = i + 1
+    prev = s[-1]
+    for i in range(len(s),30000000):
+#        print(prev,i)
+        p = prev
+        if prev in seen:
+            prev = i - seen[prev]
+        else:
+            prev = 0
+        seen[p] = i
+    print(prev)
